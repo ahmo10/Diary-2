d9fa89383ed6d8ad5fec25a0928d3888688889f6
ahmo10 <ahmey.ibra@gmail.com>
2018-07-20 23:45:27 +0300

    [feature#159171736] /for get all entries

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,jsonify, request
 
 app = Flask(__name__)
 
+#route getting every entries
+@app.route('/api/v1/entries', methods=['GET'])
+def get_entries():
+    return jsonify({'Entries': Entries})
 
 if __name__ == '__main__':
     app.run(debug=True)
